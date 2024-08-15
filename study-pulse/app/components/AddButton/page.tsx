562785473b758b81306592c6c3946e082b4072b9
Rawan Karroum <rawan.r.karroum@gmail.com>
2024-08-15 17:23:27 -0400

    added use client

--- study-pulse/app/components/AddButton/page.tsx
+++ study-pulse/app/components/AddButton/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react';
 import { Box, IconButton, Modal, Typography, TextField, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
